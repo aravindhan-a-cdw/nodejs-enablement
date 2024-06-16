92cfef607b7c14374eebdced29cd284405cc7911
Aravindhan <aravindhan.asaithambi@cdw.com>
2024-06-16 21:48:00 +0530

    Added role in signup

--- cdw-connect/config/swagger.js
+++ cdw-connect/config/swagger.js
@@ -14,6 +14,7 @@ const doc = {
       email: "kokki.kumar@cdw.com",
       gender: "male",
       password: "SuperSecret",
+      role: "user",
     },
     LoginUser: {
       email: "kokki.kumar@cdw.com",
